0c105ef261325fd91a1134e10964d101be6bd766
Hector Sanchez <hectorsanzr94@gmail.com>
2022-06-21 16:24:13 -0500

    Probando algo

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,10 +1,19 @@
-
+import propTypes from 'prop-types'
 
 import React from 'react'
 
-export const GifItem = ({title,id,url}) => {
+export const GifItem = ({title,url}) => {
   return (
-    <div className="card"><img src={url} alt={title} />
-    <p>{title}</p></div>
+    <div className="card">
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
   )
 }
+
+GifItem.propTypes = {
+  title:propTypes.string.isRequired,
+  url :propTypes.string.isRequired,
+  
+}
+
