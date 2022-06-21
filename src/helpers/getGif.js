004924350a54b3232c5112d7ce358d0bfc7e7044
Hector Sanchez <hectorsanzr94@gmail.com>
2022-06-21 15:47:03 -0500

    https

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,5 +1,5 @@
 export const getGif = async(category) =>{
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=KO3jiLLt8rHtz0NTn81OWTwxno7Tx2x7&q=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=KO3jiLLt8rHtz0NTn81OWTwxno7Tx2x7&q=${category}&limit=10`
     const resp = await fetch( url )
     const {data} = await resp.json();
 
